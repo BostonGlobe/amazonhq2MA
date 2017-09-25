6b0cbb8e3a9c62a36119d13860624551a915be9f
Yan Wu <yan.wu@globe.com>
2017-09-25 16:30:28 -0400

    delete pictures

--- src/main.js
+++ src/main.js
@@ -18,36 +18,39 @@
 	window.enterView = function() {
 
 	};
-	var sites=[{"site":"Former Showcase Cinema property","siteLat":42.689130,"siteLng":-71.143711,"siteInfo":"It is a 14-acre city-owned lot.","transit":"Commuter rail stop in Lawrence","id":1},
-        {"site":"Osgood Landing office","siteLat":42.731070,"siteLng":-71.114405,"siteInfo":"The complex is 168 acre along the Merrimack River.","transit":"Commuter rail stop in Lawrence","id":1},
-        {"site":"Whaling City Golf Course","siteLat":41.658893,"siteLng":-70.957943,"siteInfo":"It is a 300-acre city-owned site slated for redevelopment","transit":"Bus","id":2},
-        {"site":"Union Point","siteLat":42.165521,"siteLng":-70.938159,"siteInfo":"It is a 1550-acre formal navy base turned live-work-play complex","transit":"Commuter rail stop at Union Point","id":4},
-        {"site":"Assembly Square","siteLat":42.392942,"siteLng":-71.080534,"siteInfo":"It would need to be part of a regional bid","transit":"Orange Line ","id":5},
-        {"site":"Gateway Park","siteLat":42.273665,"siteLng":-71.806378,"siteInfo":"It was anchored by Worcester Polytechnic Institute","transit":"Commuter rail","id":6},
-        {"site":"Suffolk Downs","siteLat":42.394576,"siteLng":-71.010499,"siteInfo":"It is a thoroughbred race track in East Boston.","transit":"Blue Line","id":7},
-        {"site":"Billerica","siteLat":42.609995,"siteLng":-71.291785,"siteInfo":"It is a a property near the Commuter Rail, which also includes Tewksbury and a small bit of Lowell.","transit":"Blue Line","id":8}];
+	var sites=[{"site":"Former Showcase Cinema property","siteLat":42.68913,"siteLng":-71.143711,"siteType":"Potential site","siteInfo":"It is former Showcase Cinema property, a 14-acre city-owned lot.","transit":"Commuter rail stop in Lawrence","id":1,"sitePic":"http://photos.cinematreasures.org/production/photos/99764/1400530544/large.jpg?1400530544"},
+        {"site":"Osgood Landing office","siteLat":42.73107,"siteLng":-71.114405,"siteType":"Main site","siteInfo":"It is a 168-acre-parcel along the Merrimack River, the former AT&T/Lucent Technologies manufacturing complex.","transit":"Commuter rail stop in Lawrence","id":1,"sitePic":"http://www.ozzyproperties.com/wp-content/uploads/2017/02/Osgood-Landing.jpg"},
+        {"site":"Whaling City Golf Course","siteLat":41.658893,"siteLng":-70.957943,"siteType":"Potential site","siteInfo":"Tt is a 300-acre property owned by the city and is slated for redevelopment.","transit":"Bus","id":2,"sitePic":"http://wbsm.com/files/2017/05/20170419_135617.jpg?w=630&h=420&zc=1&s=0&a=t&q=89"},
+        {"site":"Union Point","siteLat":42.165521,"siteLng":-70.938159,"siteType":"Potential site","siteInfo":"The former South Weymouth naval base that sits on 1,500 acres and straddles three towns. Being turned into a mega development featuring offices, housing, and amenities like restaurants.","transit":"Commuter rail stop at Union Point","id":4,"sitePic":"https://lh3.googleusercontent.com/proxy/wJit8NIb3yhnsw3sE2XdGXGZUvfm0quJUBLBXCCHoYshmLw6WiC4my3qA7LKzASeGcxfJ1LD4SuTv6P73irlFVVhpt8Ce8TCrLHh1NjsiW0FS25GKFcVCFgma2bJQPWIas9QHy0JyMLDK46RF9xZZB-93aUpGKc=w285-h160-k-no"},
+        {"site":"Assembly Square","siteLat":42.392942,"siteLng":-71.080534,"siteType":"Potential site","siteInfo":"It would need to be part of a regional bid","transit":"Orange Line ","id":5,"sitePic":"http://worldlandscapearchitect.com/wp-content/uploads/2015/05/Baxter-1-565x375.jpg"},
+        {"site":"Gateway Park","siteLat":42.273665,"siteLng":-71.806378,"siteType":"Potential site","siteInfo":"It is a research-business development co-owned by Worcester Polytechnic Institute; CitySquare, downtown project next to Union Station","transit":"Commuter rail","id":6,"sitePic":"https://www.wpi.edu/sites/default/files/2017/04/05/WP%20Oct11%20%20255.2.jpg"},
+        {"site":"Suffolk Downs","siteLat":42.394576,"siteLng":-71.010499,"siteType":"Potential site","siteInfo":"It is a thoroughbred race track in East Boston.","transit":"Blue Line","id":7,"sitePic":"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Suffolk_Downs.jpg/250px-Suffolk_Downs.jpg"},
+        {"site":"A collection of lots","siteLat":42.609995,"siteLng":-71.291785,"siteType":"Potential site","siteInfo":"It is off Billerica Avenue, near Baker Commodities and Commuter Rail.","transit":"Commuter Rail","id":8,"sitePic":""}];
 
 	// graphic code
-	var cities = [{"city":"Haverhill","part":"3","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Commuter rail stop in Lawrence","id":1},
-        {"city":"Lawrence","part":"3","site":"National Amusements","siteLat":42.199998,"siteLng":-71.176263,"siteInfo":"14-acre city-owned lot","transit":"Commuter rail stop in Lawrence","id":1},
-        {"city":"North Andover","part":"3","site":"Osgood Landing office","siteLat":42.731070,"siteLng":-71.114405,"siteInfo":"The complex is 168 acre along the Merrimack River","transit":"Commuter rail stop in Lawrence","id":1},
-        {"city":"New Bedford","part":"1","site":"Whaling City Golf Course","siteLat":41.658893,"siteLng":-70.957943,"siteInfo":"a 300-acre city-owned site slated for redevelopment","transit":"Bus","id":2},
-        {"city":"Westfield","part":"9","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Bus","id":3},
-        {"city":"Springfield","part":"9","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Bus","id":3},
-        {"city":"Holyoke","part":"9","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Bus","id":3},
-        {"city":"Chicopee","part":"9","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Bus","id":3},
-        {"city":"Agawam","part":"9","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Bus","id":3},
-        {"city":"West Springfield","part":"9","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Bus","id":3},
-        {"city":"Northampton","part":"9","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Bus","id":3},
-        {"city":"Greenfield","part":"9","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Bus","id":3},
-        {"city":"Easthampton","part":"9","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Bus","id":3},
-        {"city":"Rockland","part":"3","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Commuter rail stop at Union Point","id":4},
-        {"city":"Weymouth","part":"3","site":"Union Point","siteLat":42.165521,"siteLng":-70.938159,"siteInfo":"1550-acre formal navy base turned live-work-play complex","transit":"Commuter rail stop at Union Point","id":4},
-        {"city":"Abington","part":"3","site":"","siteLat":null,"siteLng":null,"siteInfo":"","transit":"Commuter rail stop at Union Point","id":4},
-        {"city":"Somerville","part":"1","site":"Assembly Square","siteLat":42.392942,"siteLng":-71.080534,"siteInfo":"would need to be part of a regional bid","transit":"Orange Line ","id":5},
-        {"city":"Worcester","part":"1","site":"Gateway Park","siteLat":42.273665,"siteLng":-71.806378,"siteInfo":"anchored by Worcester Polytechnic Institute","transit":"Commuter rail","id":6},
-        {"city":"Boston","part":"1","site":"Suffolk Downs","siteLat":42.394576,"siteLng":-71.010499,"siteInfo":"","transit":"Blue Line","id":7},
-        {"city":"Billerica","part":"1","site":"Suffolk Downs","siteLat":42.609995,"siteLng":-71.291785,"siteInfo":"","transit":"Commuter Rail","id":8}];
+	var cities = [{"city":"Haverhill","part":3,"mayor":null,"cityInfo":"Plan to file a regional bid with two other cities.","site":"","siteLat":"null","siteLng":"null","siteInfo":"","transit":"Commuter rail stop in Lawrence","id":1},
+        {"city":"Lawrence","part":3,"mayor":null,"cityInfo":"Plan to file a regional bid with two other cities.","site":"Former Showcase Cinema property","siteLat":"42.689130","siteLng":"-71.143711","siteInfo":"It's a 14-acre city-owned lot.","transit":"Commuter rail stop in Lawrence","id":1},
+        {"city":"North Andover","part":3,"mayor":null,"cityInfo":"Plan to file a regional bid with two other cities.","site":"Osgood Landing office","siteLat":"42.73107","siteLng":"-71.114405","siteInfo":"It's 168-parcel along the Merrimack River, the former AT&T/Lucent Technologies manufacturing complex.","transit":"Commuter rail stop in Lawrence","id":1},
+        {"city":"New Bedford","part":1,"mayor":null,"cityInfo":"Strongly considering a bid","site":"Whaling City Golf Course","siteLat":"41.658893","siteLng":"-70.957943","siteInfo":"It's a 300-acre property owned by the city and that is slated for redevelopment","transit":"Bus","id":2},
+        {"city":"Westfield","part":9,"mayor":null,"cityInfo":"Weighing regional bid involving nine cities organized by <br/>the Economic Development Council of Western Massachusetts. ","site":"","siteLat":"null","siteLng":"null","siteInfo":"Potential sites to be determined.","transit":"Bus","id":3},
+        {"city":"Springfield","part":9,"mayor":null,"cityInfo":"Weighing regional bid involving nine cities organized by <br/>the Economic Development Council of Western Massachusetts. ","site":"","siteLat":"null","siteLng":"null","siteInfo":"Potential sites to be determined.","transit":"Bus","id":3},
+        {"city":"Holyoke","part":9,"mayor":null,"cityInfo":"Weighing regional bid involving nine cities organized by <br/>the Economic Development Council of Western Massachusetts. ","site":"","siteLat":"null","siteLng":"null","siteInfo":"Potential sites to be determined.","transit":"Bus","id":3},
+        {"city":"Chicopee","part":9,"mayor":null,"cityInfo":"Weighing regional bid involving nine cities organized by <br/>the Economic Development Council of Western Massachusetts. ","site":"","siteLat":"null","siteLng":"null","siteInfo":"Potential sites to be determined.","transit":"Bus","id":3},
+        {"city":"Agawam","part":9,"mayor":null,"cityInfo":"Weighing regional bid involving nine cities organized by <br/>the Economic Development Council of Western Massachusetts. ","site":"","siteLat":"null","siteLng":"null","siteInfo":"Potential sites to be determined.","transit":"Bus","id":3},
+        {"city":"West Springfield","part":9,"mayor":null,"cityInfo":"Weighing regional bid involving nine cities organized by <br/>the Economic Development Council of Western Massachusetts. ","site":"","siteLat":"null","siteLng":"null","siteInfo":"Potential sites to be determined.","transit":"Bus","id":3},
+        {"city":"Northampton","part":9,"mayor":null,"cityInfo":"Weighing regional bid involving nine cities organized by <br/>the Economic Development Council of Western Massachusetts. ","site":"","siteLat":"null","siteLng":"null","siteInfo":"Potential sites to be determined.","transit":"Bus","id":3},
+        {"city":"Greenfield","part":9,"mayor":null,"cityInfo":"Weighing regional bid involving nine cities organized by <br/>the Economic Development Council of Western Massachusetts. ","site":"","siteLat":"null","siteLng":"null","siteInfo":"Potential sites to be determined.","transit":"Bus","id":3},
+        {"city":"Easthampton","part":9,"mayor":null,"cityInfo":"Weighing regional bid involving nine cities organized by <br/>the Economic Development Council of Western Massachusetts. ","site":"","siteLat":"null","siteLng":"null","siteInfo":"Potential sites to be determined.","transit":"Bus","id":3},
+        {"city":"Rockland","part":3,"mayor":null,"cityInfo":"Plan to file a regional bid with two other cities.","site":"","siteLat":"null","siteLng":"null","siteInfo":"","transit":"Commuter rail stop at Union Point","id":4},
+        {"city":"Weymouth","part":3,"mayor":null,"cityInfo":"Plan to file a regional bid with two other cities.","site":"Union Point","siteLat":"42.165521","siteLng":"-70.938159","siteInfo":"The former South Weymouth naval base that sits on 1,500 acres and straddles three towns. Being turned into a mega development featuring offices, housing, and amenities like restaurants.","transit":"Commuter rail stop at Union Point","id":4},
+        {"city":"Abington","part":3,"mayor":null,"cityInfo":"Plan to file a regional bid with two other cities.","site":"","siteLat":"null","siteLng":"null","siteInfo":"","transit":"Commuter rail stop at Union Point","id":4},
+        {"city":"Somerville","part":1,"mayor":null,"cityInfo":"It would need to be part of a regional bid.","site":"Assembly Square","siteLat":"42.392942","siteLng":"-71.080534","siteInfo":"It would need to be part of a regional bid","transit":"Orange Line ","id":5},
+        {"city":"Worcester","part":1,"mayor":null,"cityInfo":"Hope to be part of state application.","site":"Gateway Park","siteLat":"42.273665","siteLng":"-71.806378","siteInfo":"a research-business development co-owned by Worcester Polytechnic Institute; CitySquare, downtown project next to Union Station","transit":"Commuter rail","id":6},
+        {"city":"Boston","part":1,"mayor":null,"cityInfo":"Plan to file a bid.","site":"Suffolk Downs","siteLat":"42.394576","siteLng":"-71.010499","siteInfo":"","transit":"Blue Line","id":7},
+        {"city":"Billerica","part":3,"mayor":null,"cityInfo":"Working on an application with two other cities.","site":"A collection of lots","siteLat":"42.609995","siteLng":"-71.291785","siteInfo":"It's off Billerica Avenue, near Baker Commodities and Commuter Rail.","transit":"Commuter Rail","id":8},
+        {"city":"Tewksbury","part":3,"mayor":null,"cityInfo":"Working on an application with two other cities.","site":"","siteLat":"","siteLng":"","siteInfo":"","transit":"Commuter rail","id":8},
+        {"city":"Lowell","part":3,"mayor":null,"cityInfo":"Working on an application with two other cities.","site":"","siteLat":"","siteLng":"","siteInfo":"","transit":"Commuter rail","id":8}];
+
 
     //var dataUrl="https://api.twitter.com/1.1/search/tweets.json?q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4";
     var satelliteStreets = "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVybWlvbmV3eSIsImEiOiJjaXZ5OWI1MTYwMXkzMzFwYzNldTl0cXRoIn0.Uxs4L2MP0f58y5U-UqdWrQ";
@@ -56,12 +59,12 @@
 
     function style(feature) {
         return {
-            fillColor: '#ffffff',
+            fillColor: '#d4d4d4',
             weight: 1,
             opacity: 0.3,
             color: '#987d66',
             dashArray: '3',
-            fillOpacity: 0.2
+            fillOpacity: 0.4
         };
     }
     // graphic code
@@ -88,7 +91,7 @@
 
 
 	sites.forEach(function (t) {
-		L.marker ([t.siteLat, t.siteLng]).bindPopup('<p><span>'+ t.site +'</span><br/>Descriptions: '+ t.siteInfo+'</p>').openPopup().addTo(map);
+		L.marker ([t.siteLat, t.siteLng]).bindPopup('<p><span>'+ t.site +'</span> ('+ t.siteType +')<br/>'+ t.siteInfo+'</p>').openPopup().addTo(map);
 	});
 
     var shpfile = new L.Shapefile('assets/towns.zip', {
@@ -132,7 +135,7 @@
         //console.log(props);
         var obj= (props)? getInfo(props["TOWN"]):{info:"Hover over a town"};
         this._div.innerHTML = '<h4>Which cities are bidding for Amazon HQ2? </h4>' +  (props ?
-            '<p>' + obj.city+'<br/>' + isTeam(obj)+haveSite(obj.id) +'</p>'
+            '<p>' + obj.city+'<br/>' + obj.cityInfo + haveSite(obj.id)+'</p>'
             : obj.info);
     };
 
@@ -151,7 +154,7 @@
     }
     function haveSite(id) {
         if(id==3){
-            return "<br/> The site is to be decided."
+            return "<br/> The sites are to be determined."
         }else{
             return [];
         }
